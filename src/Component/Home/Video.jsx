6c189729022e0afd886e2ddd3624a7c91f3a00d9
vishal-patel20 <shivam1705x@gmail.com>
2026-02-02 13:58:18 +0530

    Update Video.jsx

--- src/Component/Home/Video.jsx
+++ src/Component/Home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Video() {
   return (
     <div className='h-full w-full'>
-      <video className='h-full w-full object-cover' autoPlay muted loop src="/public/Video/hero.mp4"></video>
+      <video className='h-full w-full object-cover' autoPlay muted loop src="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1770020823~exp=1770024423~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=d76fe8b9838ad47894d94a82291840d6353ffe7cbaa10ddb9205a52b578730cb&r=dXMtZWFzdDE%3D"></video>
     </div>
   )
 }
